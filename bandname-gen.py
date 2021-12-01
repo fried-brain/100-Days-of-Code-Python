4286b36e9b4511ab25e59b5d46c4262dd69b2367
Fried Brain <workforgodspray@gmail.com>
2021-12-01 19:35:57 +0530

    Create bandname-gen.py

---
+++ bandname-gen.py
@@ -0,0 +1,5 @@
+name = input("Welcome to Band Name Generator!\nWhat is your name?\n")
+city = input(f"Hi {name}, Where do you spent your childhood?\n")
+pet = input(f"What is your pet's name, {name}?\n")
+
+print(f"Your band name could be : {city} {pet}")
