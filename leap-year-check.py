6b10e38b5f78ee4a3aec728814c1c446961cfd35
Fried Brain <workforgodspray@gmail.com>
2021-12-01 20:23:36 +0530

    Update leap-year-check.py

--- leap-year-check.py
+++ leap-year-check.py
@@ -4,10 +4,10 @@ year = int(input("Which year do you want to check? \n"))
 if year % 4 == 0:
   if year % 100 == 0:
     if year % 400 == 0:
-      print(f"{year} is a Leap year.")
+      print(f"Year {year} is a Leap year.")
     else:
-      print(f"{year} is not a Leap year.")
+      print(f"Year {year} is not a Leap year.")
   else:
-    print(f"{year} is a Leap year.")
+    print(f"Year {year} is a Leap year.")
 else:
-  print(f"{year} is not a Leap year.")
+  print(f"Year {year} is not a Leap year.")
